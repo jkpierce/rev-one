4a9ea1028b499e5a2087889bd39ed9265ab1fd13
Kevin Pierce <kevinkayaks@gmail.com>
2019-07-01 21:37:40 -0700

    almost

--- jgr/figures/tile.sh
+++ jgr/figures/tile.sh
@@ -2,5 +2,6 @@
 
 # this script makes the two figures from their components
 montage activity.pdf ele.pdf -mode Concatenate -tile 2x1 montage2.pdf
+echo "activity/elevation figure made"
 montage 1.pdf 2.pdf 3.pdf 4.pdf -mode Concatenate -tile 2x2 montage1.pdf
-
+echo "four panel resting time figure made"
